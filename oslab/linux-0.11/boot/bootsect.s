ca074efc449a29c88447b9b777626230ebcc0daa
JiazhenZhao02091 <873694994@qq.com>
2023-12-03 22:56:06 -0800

    done bootsect output.

--- oslab/linux-0.11/boot/bootsect.s
+++ oslab/linux-0.11/boot/bootsect.s
@@ -3,20 +3,26 @@ SETUPSEG=0x07e0
 SETUPLEN=2  !linus写的是4，教程是2
 entry _start
 _start:
-!屏幕输出功能
+! 屏幕输出功能
+!	"读取光标位置"
     mov ah,#0x03
     xor bh,bh
     int 0x10
-    
+!	"获取信息"
     mov bx,#0x0007
     mov cx,#32
     mov bp,#msg1
-	
+!  "移动段寄存器 es"
     mov ax,#BOOTSEG
     mov es,ax
+! 	"写字符串并移动光标"
     mov ax,#0x1301
     int 0x10
-    
+!	"设置无限循环"
+inf_loop:
+    jmp inf_loop
+
+
 !加载setup
 load_setup:
      mov dx,#0x0000
@@ -34,15 +40,14 @@ load_setup:
 ok_load_setup:
      jmpi 0,SETUPSEG !段间跳转指令  cs = SETUPSEG，ip = 0
      	
-!inf_loop:
-!    jmp inf_loop
     
 msg1:
     .byte   13,10
-    .ascii  "Forrest's OS is Loading..."
+    .ascii  "Jiazhen's OS is Loading..."
     .byte   13,10,13,10
     
 .org 510
 
+!"引导扇区正确结束的标志"
 boot_flag:
     .word   0xAA55
